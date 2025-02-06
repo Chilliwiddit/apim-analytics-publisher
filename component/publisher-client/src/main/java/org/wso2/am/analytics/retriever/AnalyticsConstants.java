736e05003710bd3e70974b7a7c83d41eec6f0ce4
Chilliwiddit <nivedhith@wso2.com>
2025-02-06 11:02:36 +0530

    Create analytics constants class

---
+++ component/publisher-client/src/main/java/org/wso2/am/analytics/retriever/AnalyticsConstants.java
@@ -0,0 +1,21 @@
+package org.wso2.am.analytics.retriever;
+
+public class AnalyticsConstants {
+
+    public static final String API_NAME = "apiName";
+    public static final String API_VERSION = "apiVersion";
+    public static final String API_UUID = "apiId";
+    public static final String TENANT_DOMAIN = "apiCreatorTenantDomain";
+    public static final String APPLICATION_NAME = "applicationName";
+    public static final String APPLICATION_OWNER = "applicationOwner";
+    public static final String COUNT = "count";
+    public static final String TENANT_DOMAIN_COL = "tenantDomains";
+    public static final String FROM = "from";
+    public static final String TO = "to";
+    public static final String TIME_FORMAT = "yyyy-MM-dd'T'HH:mm:ss";
+    public static final String TIME_ZONE = "UTC";
+    public static final String TIMEZONE_FORMAT = ".023+00:00";
+    public static final String COMPLETED = "COMPLETED";
+    public static final String SUCCESSFULL = "SUCCESSFULL";
+
+}
